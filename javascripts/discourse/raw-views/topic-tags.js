bb5c54e685ee007af65d9bfa48c674798cd27d44
David Taylor <david@taylorhq.com>
2025-04-14 12:14:46 +0100

    DEV: Remove legacy topic-list code

--- javascripts/discourse/raw-views/topic-tags.js
+++
@@ -1,3 +0,0 @@
-import EmberObject from "@ember/object";
-
-export default class TopicTags extends EmberObject {}
--- javascripts/discourse/raw-views/topic-thumbnail.js
+++
@@ -1,3 +0,0 @@
-import EmberObject from "@ember/object";
-
-export default class TopicThumbnail extends EmberObject {}
